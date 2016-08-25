3fbce79b4025a3cbed29bf8fbfa7353f9124a458
Timothy Spain <timothy.spain@diamond.ac.uk>
2016-08-25 10:01:06 +0100

    Add the necessary PHP changes to run on the VM with PHP 7

--- api/includes/class.mysql.php
+++ api/includes/class.mysql.php
@@ -272,37 +272,37 @@
             }
 
             $data = array();
-            if (strpos($query, 'SELECT') !== false) {
-                $params = array();
-                $row = array();
-                $meta = $stmt->result_metadata();
-                while ($field = $meta->fetch_field()) {
-                    array_push($params, &$row[$field->name]);
-                }
-                call_user_func_array(array($stmt, 'bind_result'), $params);
+            /* if (strpos($query, 'SELECT') !== false) { */
+            /*     $params = array(); */
+            /*     $row = array(); */
+            /*     $meta = $stmt->result_metadata(); */
+            /*     while ($field = $meta->fetch_field()) { */
+            /*         array_push($params, &$row[$field->name]); */
+            /*     } */
+            /*     call_user_func_array(array($stmt, 'bind_result'), $params); */
 
-                while ($stmt->fetch()) {
-                    $c = array();
-                    // Oracle returns all values as strings - Need to be consistent :(
-                    foreach ($row as $key => $val) $c[strtoupper($key)] = $val === null ? null : strval($val);
-                    $data[] = $c;
-                }
-            }
+            /*     while ($stmt->fetch()) { */
+            /*         $c = array(); */
+            /*         // Oracle returns all values as strings - Need to be consistent :( */
+            /*         foreach ($row as $key => $val) $c[strtoupper($key)] = $val === null ? null : strval($val); */
+            /*         $data[] = $c; */
+            /*     } */
+            /* } */
 
             if ($this->debug) print_r(array('rows', sizeof($data)));
 
             // Need mysqlnd for this :(
-            // $result = $stmt->get_result();
+            $result = $stmt->get_result();
             
-            // $data = array();
+            $data = array();
             
-            // if ($result) {
-            //     if($result->num_rows > 0) {
-            //         while($row = $result->fetch_assoc()) {
-            //             array_push($data, array_change_key_case($row, CASE_UPPER));
-            //         }
-            //     }
-            // }
+            if ($result) {
+                if($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        array_push($data, array_change_key_case($row, CASE_UPPER));
+                    }
+                }
+            }
             
             $stmt->close();
 
