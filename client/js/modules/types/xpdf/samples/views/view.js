5ce2cbbced9aa588f1600cd4b2db7acf8a9043dd
Timothy Spain <timothy.spain@diamond.ac.uk>
2016-10-11 15:38:30 +0100

    A basic Sample view for XPDF, reusing common elements from generic view

--- client/js/modules/types/xpdf/samples/views/view.js
+++ client/js/modules/types/xpdf/samples/views/view.js
@@ -3,11 +3,47 @@
  */
 
 define(["marionette",
+        "utils/editable",
+        "collections/datacollections",
+        "modules/dc/views/getdcview",
         "tpl!templates/types/xpdf/sample.html"
         ], function(Marionette,
+        		Editable,
+        		DCCol,
+        		GetDCView,
         		template) {
 	return Marionette.LayoutView.extend({
 		className: "content",
 		template: template,
+		
+		regions: {
+			history: '.history',
+		},
+		
+		initialize: function(options) {
+			// bind the validation
+			// Backbone.Validation.bind(this);
+			
+			// Data collections for this sample, that is where the sample ID 
+			// (sid) mathches that of this sample
+			this.dcs = new DCCol(null, { queryParams: {sid: this.model.get("BLSAMPLEID"), pp:5} });
+			this.dcs.fetch();
+			
+		},
+		
+		onRender: function() {
+			// Create the editable fields
+			// Name
+			// Comment
+			// Code
+			var edit = new Editable( { model: this.model, el: this.$el });
+			edit.create("NAME", "text");
+			edit.create("COMMENTS", "text");
+			edit.create("CODE", "text");
+			
+			// Show the Data Collections in the history region
+			this.history.show(GetDCView.DCView.get(app.type, { model: this.model, collection: this.dcs, params: { visit: null }, noPageUrl: true, noFilterUrl: true, noSearchUrl: true}));
+		}
+		
 	});
 });
