1a0b3d2e9e47226694891c10dfdb18cf6da09292
Timothy Spain <timothy.spain@diamond.ac.uk>
2016-10-12 15:37:09 +0100

    Unmocked files of the first attempt to add a phase table.

---
+++ client/js/modules/types/xpdf/samples/views/proteinlist.js
@@ -0,0 +1,28 @@
+/**
+ * XPDF phase list
+ */
+
+define(["marionette",
+        "modules/samples/views/proteinlist"
+        ], function(Marionette,
+        		ProteinList) {
+	
+	return ProteinList.extend({
+		title: "Phase",
+		
+		columns: [
+			{name: "NAME", label: "Name", cell: "string", editable: false},
+			{name: "ACRONYM", label: "Identifier", cell: "string", editable: false},
+			{name: "MOLECULARMASS", label: "Molecular Mass", cell: "string", editable: false},
+			{name: "COMPOSITION", label: "Composition", cell: "string", editable: false},
+			{name: "XDENSITY", label: "Crys. Density", cell: "string", editable: false},
+			{name: "ABUNDANCE", label: "Fraction", cell: "string", editable: false},
+		],
+	
+		hiddenColumns: [],
+		
+		intialize: function(options) {
+			ProteinList.initialize(options);
+		},
+	})
+});
