ddaa68c0c7bf72f09cdcde04e60c97b487298361
Timothy Spain <timothy.spain@diamond.ac.uk>
2016-11-14 13:38:21 +0000

    Make the phase properties editable and viewable.

--- client/js/modules/types/xpdf/samples/views/phasetableview.js
+++ client/js/modules/types/xpdf/samples/views/phasetableview.js
@@ -17,19 +17,19 @@ define([
                   {name: "NAME", label: "Name", cell: "string", editable: false},
                   {name: "ACRONYM", label: "Identifier", cell: "string", editable: false},
                   {name: "MOLECULARMASS", label: "Molecular Mass", cell: "string", editable: false},
-                  {name: "COMPOSITION", label: "Composition", cell: "string", editable: false},
+                  {name: "SEQUENCE", label: "Composition", cell: "string", editable: false},
 //      			{name: "XDENSITY", label: "Crys. Density", cell: "string", editable: false},
                   {name: "ABUNDANCE", label: "Fraction", cell: "string", editable: false},
                   ],
                   
                   // Set the row to be clickable, and for the click to display
                   // the phase information page
-//                  backgrid: {
-//                	  row: table.ClickableRow.extend({
-//                			  event: "proteins:view",
-//                			  argument: "PROTEINID",
-//                			  cookie: true,
-//                			  }),
-//                  },
+                  backgrid: {
+                	  row: table.ClickableRow.extend({
+                			  event: "proteins:view",
+                			  argument: "PROTEINID",
+                			  cookie: true,
+                			  }),
+                  },
 	});
 });
