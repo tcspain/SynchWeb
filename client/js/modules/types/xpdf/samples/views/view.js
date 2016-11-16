04744320232bc4b10de21756413da29fdf3f066e
Timothy Spain <timothy.spain@diamond.ac.uk>
2016-11-16 13:26:36 +0000

    Display all phases, primary and component, on the XPDF sample view page.

--- client/js/modules/types/xpdf/samples/views/view.js
+++ client/js/modules/types/xpdf/samples/views/view.js
@@ -38,6 +38,8 @@ define(["marionette",
 			// bind the validation
 			Backbone.Validation.bind(this);
 			
+			this.model.initialize(); // This is a terrible idea?
+			
 			// Data collections for this sample, that is where the sample ID 
 			// (sid) mathches that of this sample
 			this.dcs = new DCCol(null, { queryParams: {sid: this.model.get("BLSAMPLEID"), pp:5} });
@@ -68,25 +70,61 @@ define(["marionette",
 			// Show the phases in the "phases" region
 //			this.phases.show(new PhaseView({ model: this.model, collection: this.phaseCollection}));
 
-			var primaryPhase = new Phase({PROTEINID: this.model.get("PROTEINID")});
-			primaryPhase.fetch({
-				success: function() {
-					self.phaseCollection.add(primaryPhase);
-					self.phaseCollection.add(self.model.get("Components"));
-					self.drawPhaseTable();
-				},
-				error: function() {
-					console.log("Could not get primary phase for "+self.model.get("BLSAMPLEID"));
-				},
-			});
+			// Get the models for all the phases, more or less in parallel
+			this.getAllPhases(this.drawPhaseTable);
+			
+//			var primaryPhase = new Phase({PROTEINID: this.model.get("PROTEINID")});
+//			primaryPhase.fetch({
+//				success: function() {
+//					self.phaseCollection.add(primaryPhase);
+//					self.phaseCollection.add(self.model.get("Components"));
+//					self.drawPhaseTable();
+//				},
+//				error: function() {
+//					console.log("Could not get primary phase for "+self.model.get("BLSAMPLEID"));
+//				},
+//			});
 			
 			// Show the add phases hidden form in the "newphase" region
 			this.newphase.show(new NewPhaseView({"CRYSTALID" : this.model.get("CRYSTALID")}));
 			
 		},
 		
-		drawPhaseTable: function() {
-			this.phases.show(new PhaseTableView({ collection: this.phaseCollection, loading: true}));
+		drawPhaseTable: function(self) {
+			console.log("Drawing phase table");
+			self.phases.show(new PhaseTableView({ collection: self.phaseCollection, loading: true}));
+		},
+		
+		getAllPhases: function(successFunction) {
+			// Get the IDs of the primary phase and all secondary components
+			var primaryID = this.model.get("PROTEINID");
+			var phaseIDs;
+			var self = this;
+			
+			this.model.updateComponentIds();
+			phaseIDs = this.model.get("COMPONENTIDS");
+			
+			phaseIDs.unshift(primaryID);
+			console.log("Found "+phaseIDs.length+" components");
+			
+			// For each ID, fetch the data, and add to the phase collection
+			_.each(phaseIDs, function(element, index, list) {
+				var phase = new Phase({PROTEINID: element});
+				phase.fetch({
+					success: function() {
+						self.phaseCollection.add(phase);
+						if (self.phaseCollection.length == list.length) {
+							console.log("Success! Found all phases");
+							successFunction(self);
+						}
+						console.log("Would add phase "+element+" to the phase table for sample "+self.model.get("BLSAMPLEID"));
+					},
+					error: function() {
+						console.log("Could not get phase information for "+element);
+					}
+				});
+			}, {});
+			
 		},
 		
 		
