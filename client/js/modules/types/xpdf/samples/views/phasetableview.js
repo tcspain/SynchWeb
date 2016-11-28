08787d735a46f720b8c16b800e945965d9de0dcc
Timothy Spain <timothy.spain@diamond.ac.uk>
2016-11-28 10:03:52 +0000

    Break the phase removal code into a separate function.

--- client/js/modules/types/xpdf/samples/views/phasetableview.js
+++ client/js/modules/types/xpdf/samples/views/phasetableview.js
@@ -14,6 +14,51 @@ define([
         		Sample
         		) {	
 	
+	var removePhase = function(sample, phaseId) {
+		// Excuse me, Mr. Sample while I interfere with your intimate components
+		// Get the primary and component phase IDs
+		var sampleComponentIds = sample.get("COMPONENTIDS");
+		var primaryPhaseId = sample.get("PROTEINID");
+		
+		// If there is only a primary phase, then do not remove anything.
+		if (sampleComponentIds.length <= 0)
+			return;
+		
+		var saveParameters = {};
+		
+		// Choose between primery and component phase to be removed
+		if (primaryPhaseId === phaseId) {
+			// Remove the primary phase, and replace by the first component phase 
+		
+			// Get the ID of the first component. Assign it to
+			// removed Phase component, so that is can be
+			// subsequently removed from the components
+			phaseId = sample.get("COMPONENTIDS")[0];
+			saveParameters.PROTEINID = phaseId;
+			// saveParameters.PRIMARYAMOUNT = theSample.get("COMPONENTAMOUNTS")[0];
+		}
+			
+		if (_.contains(sampleComponentIds, phaseId.toString())) {
+			// Wrapper for splice to return the spliced array
+			var removeReturn = function(array, index) {
+				if (!$.isArray(array))
+					return null;
+				array.splice(index, 1);
+				return array;
+			};
+			
+			var componentIndex = _.indexOf(sampleComponentIds, phaseId.toString());
+			
+			saveParameters.COMPONENTIDS = removeReturn(sample.get("COMPONENTIDS"), componentIndex);
+			saveParameters.COMPONENTACRONYMS = removeReturn(sample.get("COMPONENTACRONYMS"), componentIndex);
+			saveParameters.COMPONENTAMOUNTS = removeReturn(sample.get("COMPONENTAMOUNTS"), componentIndex);
+			// Save the sample with the component removed
+			sample.save(saveParameters, {});
+		}
+			
+		
+	};
+	
 	var RemoveCell = table.TemplateCell.extend({
 		events: {
 			"click a.remlink": "doRemove",
@@ -31,50 +76,7 @@ define([
 			var removedPhaseId = this.model.get("PROTEINID");
 			theSample.fetch({
 				success: function(model, response, options) {
-					
-					// Excuse me, Mr. Sample while I interfere with your intimate components
-					// Get the primary and component phase IDs
-					var sampleComponentIds = theSample.get("COMPONENTIDS");
-					var primaryPhaseId = theSample.get("PROTEINID");
-					
-					// If there is only a primary phase, then do not remove anything.
-					if (sampleComponentIds.length <= 0)
-						return;
-					
-					var saveParameters = {};
-					
-					// Choose between primery and component phase to be removed
-					if (primaryPhaseId === removedPhaseId) {
-						// Remove the primary phase, and replace by the first component phase 
-					
-						// Get the ID of the first component. Assign it to
-						// removed Phase component, so that is can be
-						// subsequently removed from the components
-						removedPhaseId = theSample.get("COMPONENTIDS")[0];
-						saveParameters.PROTEINID = removedPhaseId;
-						// saveParameters.PRIMARYAMOUNT = theSample.get("COMPONENTAMOUNTS")[0];
-					}
-						
-						
-						
-					if (_.contains(sampleComponentIds, removedPhaseId.toString())) {
-						// Wrapper for splice to return the spliced array
-						var removeReturn = function(array, index) {
-							if (!$.isArray(array))
-								return null;
-							array.splice(index, 1);
-							return array;
-						};
-						
-						var componentIndex = _.indexOf(sampleComponentIds, removedPhaseId.toString());
-						
-						saveParameters.COMPONENTIDS = removeReturn(theSample.get("COMPONENTIDS"), componentIndex);
-						saveParameters.COMPONENTACRONYMS = removeReturn(theSample.get("COMPONENTACRONYMS"), componentIndex);
-						saveParameters.COMPONENTAMOUNTS = removeReturn(theSample.get("COMPONENTAMOUNTS"), componentIndex);
-						// Save the sample with the component removed
-						theSample.save(saveParameters, {});
-					}
-						
+					removePhase(theSample, removedPhaseId);
 				},
 				error: function(model, response, options){
 					// Cannot get the sample with that ID? Do nothing
