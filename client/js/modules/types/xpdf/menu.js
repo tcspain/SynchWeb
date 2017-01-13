cd34d92b0f05608cc251f416ba3108c941938bff
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-01-13 12:01:58 +0000

    Menu for XPDF proposal pages

---
+++ client/js/modules/types/xpdf/menu.js
@@ -0,0 +1,27 @@
+/**
+ * The menu specification for XPDF pages 
+ */
+
+define([], function() {
+	
+	return {
+		menu: {
+			dc: "View All Data",
+			visits: "Visits",
+			assign: "Assign Sample Changers",
+			dewars: "Samples Sets",
+			containers: "Sample Changers",
+			samples: "Samples",
+			proteins: "Phases",
+		
+		},
+		
+		extra: {
+		
+		},
+		
+		admin: {
+			
+		},
+	};
+});
