dacaf52f5ec23534a4e2d00dc2c37efee09c92bb
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-01-13 10:16:46 +0000

    Remove the forced setting of the xpdf proposal type.

--- client/js/app.js
+++ client/js/app.js
@@ -287,8 +287,7 @@ function(Backbone, Marionette, _, $, HeaderView, SideBarView, DialogRegion, Logi
         var proposal = new Proposal({ PROPOSAL: prop })
         proposal.fetch({
             success: function() {
-//                app.type = proposal.get('TYPE'),
-            	app.type = "xpdf", // FIXME Revert to using the true proposal type
+                app.type = proposal.get('TYPE'),
                 require(['modules/types/'+proposal.get('TYPE')+'/menu'], function(menus) {
                     proposal.set('MENUS', menus)
                     app.prop = prop
