0e90cb432dfba81681e62b6081caee15ce048956
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-01-31 16:15:58 +0000

    Add per-type logic for the 'Add Container' view

--- client/js/modules/shipment/views/getshipmentview.js
+++ client/js/modules/shipment/views/getshipmentview.js
@@ -6,17 +6,25 @@ define ([
          "views/getview",
          
          "modules/shipment/views/shipment",
+         "modules/shipment/views/container",
+         "modules/shipment/views/containerplate",
+         "modules/shipment/views/containeradd",
          "modules/shipment/views/dewarreg",
          
          "modules/types/xpdf/shipment/views/shipment",
+         "modules/types/xpdf/shipment/views/stageadd",
          "modules/types/xpdf/shipment/views/dewarreg",
          ], function(
         GetView,
         
         ShipmentView,
+        ContainerView,
+        ContainerPlateView,
+        ContainerAddView,
         DewarRegView,
         
         XpdfShipmentView,
+        XpdfStageAdd,
         XpdfDewarRegView
          ) {
 	
@@ -28,6 +36,25 @@ define ([
 				xpdf: XpdfShipmentView,
 			},
 			default: ShipmentView,
+		}),
+		
+		ContainerPlateView: new GetView({
+//			views: {
+//				mx: ContainerPlateView,
+//				xpdf: ContainerPlateView,
+//			},
+			default: ContainerPlateView,
+		}),
+		
+		ContainerView: new GetView({
+			default: ContainerView,
+		}),
+		
+		ContainerAddView: new GetView({
+			views: {
+				xpdf: XpdfStageAdd,
+			},
+			default: ContainerAddView,
 		}),
 		
 		DewarRegView : new GetView({
