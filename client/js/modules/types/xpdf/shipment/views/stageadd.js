bf5fe89d0f8df24791162458f82af305b52c6fc0
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-02-01 09:51:30 +0000

    Fix the undefined value of this.group

--- client/js/modules/types/xpdf/shipment/views/stageadd.js
+++ client/js/modules/types/xpdf/shipment/views/stageadd.js
@@ -9,6 +9,7 @@ define([
         'modules/shipment/views/plate',
         'modules/shipment/views/singlesample',
         "modules/types/xpdf/shipment/collections/stagetypes",
+        "modules/imaging/views/screencomponentgroup",
         "tpl!templates/types/xpdf/shipment/stageadd.html",
         'tpl!templates/shipment/sampletablenew.html',
         'tpl!templates/shipment/sampletablerownew.html'
@@ -19,7 +20,8 @@ define([
         	    PlateView,
         	    SingleSample,
         	    XpdfStageTypes,
-        		template,
+        	    ScreenGroupView,
+        	    template,
         		table,
         		row
         ) {
@@ -34,7 +36,6 @@ define([
 		
 		// Override the setType function with XPDF specific gubbins
 		setType: function(e) {
-			console.log("Set XPDF stage type!");
 			this.type = this.ctypes.findWhere({name: this.ui.type.val()});
 			this.type.set({isSelected: true});
 			this.model.set({
@@ -46,25 +47,28 @@ define([
 			// Treat sample changers seperately from single samples
 			if (this.type.get("capacity") > 1) {
 				this.puck.$el.css("width", app.mobile() ? "100%" : "50%");
+	            this.puck.show(new PlateView({ collection: this.samples, type: this.type, showValid: true }))
 				this.stable = new SampleTableView({ proteins: this.proteins, gproteins: this.gproteins, collection: this.samples, childTemplate: row, template: table, type: 'non-xtal' });
                 this.table.show(this.stable);
                 this.singlesample = new SingleSample({ proteins: this.proteins, gproteins: this.gproteins, platetypes: this.ctypes, samples: this.samples });
 //                this.single.show(this.singlesample);
-                this.single.empty()
+                this.single.empty();
                 this.ui.pc.show()
 			} else {
 				this.puck.$el.css("width", app.mobile() ? "50%" : "25%");
-                this.table.empty()
-                if (typeof this.stable != "undefined") this.stable.destroy()
+				this.puck.empty();
+                this.table.empty();
+                if (typeof this.stable != "undefined") this.stable.destroy();
                 this.singlesample = new SingleSample({ proteins: this.proteins, gproteins: this.gproteins, platetypes: this.ctypes, samples: this.samples });
                 this.single.show(this.singlesample);
                 this.ui.pc.hide()
 
 			}
 			
-            this.puck.show(new PlateView({ collection: this.samples, type: this.type, showValid: true }))
-            this.buildCollection()
-            this.grp.empty()
+			// define the group before building the collection 
+            this.group = new ScreenGroupView({ components: this.screencomponents, editable: false });
+            this.grp.empty();
+            this.buildCollection();
             this.$el.find('li.plate').show()
             this.$el.find('li.pcr').hide()
 
