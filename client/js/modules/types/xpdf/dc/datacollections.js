5f63ac34378748797b0d037185c75b8509b7f99d
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-03-23 14:52:23 +0000

    Add and use a new dialog pop-up for creating a new sample.

--- client/js/modules/types/xpdf/dc/datacollections.js
+++ client/js/modules/types/xpdf/dc/datacollections.js
@@ -5,12 +5,14 @@ define([
         "modules/dc/datacollections",
         "modules/types/gen/dc/dclist",
         "modules/types/xpdf/samples/views/newemptysample",
+        "modules/types/xpdf/samples/views/newsamplefromphasepopup",
         "tpl!templates/types/xpdf/dc/dclist.html",
         ],
 function(
 		DataCollections,
 		DCList,
 		newEmptySample,
+		newSampleFromPhasePopupView,
 		template) {
 	return DataCollections.extend({
 		dcListView: DCList,
@@ -23,7 +25,9 @@ function(
 		},
 	
 		newSample: function() {
-			newEmptySample.run(this.model.get('VISIT'));
+//			newEmptySample.run(this.model.get('VISIT'));
+			var newSampleView = new newSampleFromPhasePopupView({visitId: this.model.get("VISIT")});
+			app.dialog.show(newSampleView);
 		}
 	});
 });
