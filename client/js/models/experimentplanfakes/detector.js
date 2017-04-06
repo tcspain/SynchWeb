f5d7c8d5f015bb8e5cfa80b86d987874031e34aa
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-04-06 15:14:54 +0100

    Add the detector model and collection

---
+++ client/js/models/experimentplanfakes/detector.js
@@ -0,0 +1,48 @@
+/**
+ * Fake model for Detectors
+ */
+
+define([	
+	"backbone",
+	"jquery.mockjax",
+	], function(
+			Backbone,
+			mockjax
+	) {
+	var Detector = Backbone.Model.extend({
+		idAttribute: "DETECTORID",
+		urlRoot: "/detector",
+
+	});
+		
+	mockjax([
+		{
+			url: "/api/detector/1",
+			responseText: {
+				DETECTORID: "1",
+				TYPE: "PDF",
+				MANUFACTURER: "Perkin-Elemer",
+				MODEL: "4K",
+				DISTANCEMIN: "0.3",
+				DISTANCEMAX: "3.0",
+				DENSITY: "4.5",
+				COMPOSITION: "CsI",
+			},
+		}, {
+			url: "/api/detector/2",
+			responseText: {
+				DETECTORID: "2",
+				TYPE: "Bragg",
+				MANUFACTURER: "Perkin-Elemer",
+				MODEL: "4K",
+				DISTANCEMIN: "0.3",
+				DISTANCEMAX: "3.0",
+				DENSITY: "4.5",
+				COMPOSITION: "CsI",
+			},
+		},
+
+		]);
+		
+	return Detector;
+});
