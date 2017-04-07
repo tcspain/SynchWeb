9b5aba9345f59e62531ef86ba635f2bcbc142493
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-04-07 09:37:15 +0100

    Mock scan parameter models and collections.

---
+++ client/js/collections/experimentplanfakes/scanparametersmodels.js
@@ -0,0 +1,137 @@
+/**
+ * Fake Collection for scan parameters
+ */
+
+define([	
+	"backbone.paginator",
+	"jquery.mockjax",
+	
+	"models/experimentplansfakes/scanparametersmodel",
+	], function(
+			PageableCollection,
+			mockjax,
+			
+			ScanParametersModel
+	) {
+	var ScanParametersCollection = PageableCollection.extend({
+		dataCollectionPlanId: null,
+		model: ScanParametersModel,
+		url: function() {
+			return "/scanparam" + (this.dataCOllectionPlanId ? "/dcplan/"+this.dataCollectionPlanId : "");
+		},
+		
+		state: {
+			pageSize: 15,
+		},
+	});
+	
+	// Parameter Models by DataCollectionPlan
+	mockjax([
+		{
+			url: "/api/scanparam/dcplan/1",
+			responseText: [
+				{
+					SCANPARAMETERSMODELID: "1",
+					SCANPARAMTERSSERVICEID: "1",
+					DATACOLLECTIONPLANID: "1",
+					MODELNUMBER: "1",
+					START: "400",
+					STOP: "600",
+					STEP: "25",
+					ARRAY: "400 K to 600 K in 25 K steps",
+				},
+				],
+		}, {
+			url: "/api/scanparam/dcplan/2",
+			responseText: [
+			],
+		}, {
+			url: "/api/scanparam/dcplan/3",
+			responseText: [
+				{
+					SCANPARAMETERSMODELID: "2",
+					SCANPARAMTERSSERVICEID: "1",
+					DATACOLLECTIONPLANID: "3",
+					MODELNUMBER: "1",
+					START: "400",
+					STOP: "650",
+					STEP: "50",
+					ARRAY: "400 K to 650 K in 50 K steps",
+				}, {
+					SCANPARAMETERSMODELID: "3",
+					SCANPARAMTERSSERVICEID: "2",
+					DATACOLLECTIONPLANID: "3",
+					MODELNUMBER: "2",
+					START: "80",
+					STOP: "200",
+					STEP: "20",
+					ARRAY: "Why run the cryo and the blower at the same time?",
+				},
+			],
+		}, {
+			url: "/api/scanparam/dcplan/4",
+			responseText: [
+				{
+					SCANPARAMETERSMODELID: "4",
+					SCANPARAMTERSSERVICEID: "1",
+					DATACOLLECTIONPLANID: "4",
+					MODELNUMBER: "1",
+					START: "400",
+					STOP: "650",
+					STEP: "50",
+					ARRAY: "400 K to 650 K in 50 K steps",
+				},
+			],
+
+		},
+		]);
+	
+	// All scanParametersModels
+	mockjax({
+		url: "/api/scanparam",
+		responseText: [
+			{
+				SCANPARAMETERSMODELID: "1",
+				SCANPARAMTERSSERVICEID: "1",
+				DATACOLLECTIONPLANID: "1",
+				MODELNUMBER: "1",
+				START: "400",
+				STOP: "600",
+				STEP: "25",
+				ARRAY: "400 K to 600 K in 25 K steps",
+			}, {
+				SCANPARAMETERSMODELID: "2",
+				SCANPARAMTERSSERVICEID: "1",
+				DATACOLLECTIONPLANID: "3",
+				MODELNUMBER: "1",
+				START: "400",
+				STOP: "650",
+				STEP: "50",
+				ARRAY: "400 K to 650 K in 50 K steps",
+			}, {
+				SCANPARAMETERSMODELID: "3",
+				SCANPARAMTERSSERVICEID: "2",
+				DATACOLLECTIONPLANID: "3",
+				MODELNUMBER: "2",
+				START: "80",
+				STOP: "200",
+				STEP: "20",
+				ARRAY: "Why run the cryo and the blower at the same time?",
+			}, {
+				SCANPARAMETERSMODELID: "4",
+				SCANPARAMTERSSERVICEID: "1",
+				DATACOLLECTIONPLANID: "4",
+				MODELNUMBER: "1",
+				START: "400",
+				STOP: "650",
+				STEP: "50",
+				ARRAY: "400 K to 650 K in 50 K steps",
+			},
+		],
+	});
+	
+	
+	
+	return ScanParametersCollection;
+	
+});
---
+++ client/js/collections/experimentplanfakes/scanparametersservices.js
@@ -0,0 +1,39 @@
+/**
+ * Fake Collection for the ScanParametersServices
+ */
+
+define([
+	"backbone.paginator",
+	"jquery.mockjax",
+
+	"models/experimentplanfakes/scanparametersservice",
+	], function(
+			PageableCollection,
+			mockjax,
+			ScanParametersService
+	) {
+		var Services = PageableCollection.extend({
+			model:ScanParametersService,
+			url: "/scanparam/service",
+			state: {
+				pageSize: 5,
+			},
+		});
+		
+		mockjax({
+			url: "/api/scanparam/service",
+			responseText: [
+				{
+					SCANPARAMETERSSERVICEID: "1",
+					NAME: "Hot air blower",
+					DESCRIPTION: "A hot air blower, capable of operating from room temperature to 600 K.",
+				}, {
+					SCANPARAMETERSSERVICEID: "2",
+					NAME: "Cryostat",
+					DESCRIPTION: "A cooler capable of operating between 77 K and 20 K below room temperature.",
+				}
+			]
+		})
+		
+		return Services;
+});
