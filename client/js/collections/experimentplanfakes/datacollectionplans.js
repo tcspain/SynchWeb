5ca55cd9f84e7949d762a65a7524d7f6d36ac736
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-06-28 10:17:06 +0100

    Revert "Add data collection plans ordered by order in the experiments, rather than database id"
    This reverts commit c0955883cf7c73347cd49cb76d61292f61e59d34.

--- client/js/collections/experimentplanfakes/datacollectionplans.js
+++ client/js/collections/experimentplanfakes/datacollectionplans.js
@@ -13,13 +13,24 @@ define([
 			DataCollectionPlan
 	) {
 	var DCPlans = PageableCollection.extend({
+		blSampleId: null,
 		model: DataCollectionPlan,
-		url: "/dcplan",
+		url: function() {
+			return "/dcplan" + (this.blSampleId ? "/sample/" + this.blSampleId : "");
+		},
 		
 		state: {
 			pageSize: 15,
 		},
 
+		/*
+		 * options
+		 * options.sampleId: get the data collections for this sample instance
+		 */
+		initialize: function(options) {
+			if (options && options.sampleId) this.blSampleId = options.sampleId;
+		},
+	
 	});
 	
 	/*
@@ -61,5 +72,62 @@ define([
 		]
 	});
 
+	
+	mockjax({
+		url: /^\/api\/dcplan\/sample\/([\d]*)$/,
+		urlParams: ["sampleId"],
+		response: function(settings) {
+			if (settings.urlParams.sampleId === "398867") 
+				this.responseText = [
+					{
+						DIFFRACTIONPLANID: "1",
+						WAVELENGTH: "0.163",
+						PREFERREDBEAMSIZEX: "0.07",
+						PREFERREDBEAMSIZEY: "0.07",
+						MONOBANDWIDTH: "0.1",
+						ORDER: "1",
+					}, {
+						DIFFRACTIONPLANID: "2",
+						WAVELENGTH: "0.207",
+						PREFERREDBEAMSIZEX: "0.07",
+						PREFERREDBEAMSIZEY: "0.07",
+						MONOBANDWIDTH: "0.1",
+						ORDER: "3",
+					},
+					];
+			else if (settings.urlParams.sampleId === "398869")
+				this.responseText = [
+					{
+						DIFFRACTIONPLANID: "2",
+						WAVELENGTH: "0.207",
+						PREFERREDBEAMSIZEX: "0.07",
+						PREFERREDBEAMSIZEY: "0.07",
+						MONOBANDWIDTH: "0.1",
+						ORDER: "2",
+					}, {
+						DIFFRACTIONPLANID: "3",
+						WAVELENGTH: "0.207",
+						PREFERREDBEAMSIZEX: "0.07",
+						PREFERREDBEAMSIZEY: "0.07",
+						MONOBANDWIDTH: "0.1",
+						ORDER: "4",
+					},
+					];
+			else if (settings.urlParams.sampleId === "398865")
+				this.responseText = [
+					{
+						DIFFRACTIONPLANID: "4",
+						WAVELENGTH: "0.288",
+						PREFERREDBEAMSIZEX: "0.07",
+						PREFERREDBEAMSIZEY: "0.07",
+						MONOBANDWIDTH: "0.1",
+						ORDER: "5",
+					},
+					];
+			else
+				this.responseText = [];
+		},
+	});
+	
 	return DCPlans;
 });
