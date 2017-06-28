2c1adaef33f64945d0ac2c7efac6cf432534dedf
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-06-28 10:17:20 +0100

    Revert "Add order to the data collections retrieved by sample id"
    This reverts commit 3b4de1b3caf734c06eb5f8ab8f68173be2b1e1b3.

--- client/js/collections/experimentplanfakes/datacollectionplans.js
+++ client/js/collections/experimentplanfakes/datacollectionplans.js
@@ -76,14 +76,6 @@ define([
 						PREFERREDBEAMSIZEX: "0.07",
 						PREFERREDBEAMSIZEY: "0.07",
 						MONOBANDWIDTH: "0.1",
-						ORDER: "1",
-					}, {
-						DIFFRACTIONPLANID: "2",
-						WAVELENGTH: "0.207",
-						PREFERREDBEAMSIZEX: "0.07",
-						PREFERREDBEAMSIZEY: "0.07",
-						MONOBANDWIDTH: "0.1",
-						ORDER: "3",
 					},
 					];
 			else if (settings.sampleId === "398869")
@@ -94,14 +86,12 @@ define([
 						PREFERREDBEAMSIZEX: "0.07",
 						PREFERREDBEAMSIZEY: "0.07",
 						MONOBANDWIDTH: "0.1",
-						ORDER: "2",
 					}, {
 						DIFFRACTIONPLANID: "3",
 						WAVELENGTH: "0.207",
 						PREFERREDBEAMSIZEX: "0.07",
 						PREFERREDBEAMSIZEY: "0.07",
 						MONOBANDWIDTH: "0.1",
-						ORDER: "4",
 					},
 					];
 			else if (settings.sampleId === "398865")
@@ -112,7 +102,6 @@ define([
 						PREFERREDBEAMSIZEX: "0.07",
 						PREFERREDBEAMSIZEY: "0.07",
 						MONOBANDWIDTH: "0.1",
-						ORDER: "5",
 					},
 					];
 			else
