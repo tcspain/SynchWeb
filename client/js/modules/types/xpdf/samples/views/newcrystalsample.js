2d7efda5ed489eb838ef376554498ff06810b38b
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-08-30 17:57:54 +0100

    Add the phase table to the new sample page.

--- client/js/modules/types/xpdf/samples/views/newcrystalsample.js
+++ client/js/modules/types/xpdf/samples/views/newcrystalsample.js
@@ -6,14 +6,22 @@
 define([
 	"views/form",
 	"models/crystal",
+	"collections/proteins",
+	"modules/types/xpdf/samples/views/crystalphasetable",
 	"tpl!templates/types/xpdf/samples/newcrystalsample.html"
 	], function(
 			FormView,
 			Crystal,
+			Phases,
+			CrystalPhaseTable,
 			template
 	) {
 	return FormView.extend({
 		template: template,
+		
+		regions: {
+			phase_table: ".phase_table",
+		},
 		
 		createModel: function() {
 			this.model = new Crystal();
@@ -35,6 +43,10 @@ define([
 			
 			// call FormView.onSubmit()
 			FormView.prototype.onSubmit.apply(this, e);
+		},
+		
+		onRender: function(e) {
+			this.phase_table.show(new CrystalPhaseTable({collection: new Phases()}));
 		}
 		
 	});
