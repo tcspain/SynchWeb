23f96ab4a71ab438b73eb8adbdf5f56be0ca8710
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-09-01 16:06:12 +0100

    Add a muxer-demuxer to display the phases.

--- client/js/modules/types/xpdf/samples/views/crystalsampleview.js
+++ client/js/modules/types/xpdf/samples/views/crystalsampleview.js
@@ -7,12 +7,14 @@ define([
 	"modules/types/xpdf/samples/views/crystalphasetable",
 	"models/protein",
 	"collections/proteins",
+	"modules/types/xpdf/utils/componentmudex",
 	"tpl!templates/types/xpdf/samples/crystalsampleview.html"
 	], function(
 		Marionette,
 		CrystalPhaseTable,
 		Phase,
 		Phases,
+		ComponentMudex,
 		template
 	) {
 	return Marionette.LayoutView.extend({
@@ -24,32 +26,9 @@ define([
 		},
 		
 		initialize: function(options) {
+			this.mudex = new ComponentMudex(this.model);
 			
-			// create the collection of phases from the primary protein data 
-			// and component arrays
-			var phases = new Phases();
-			
-			var primary = new Phase();
-			primary.set({"PROTEINID": this.model.get("PROTEINID"),
-				"ACRONYM": this.model.get("ACRONYM"),
-				"ABUNDANCE": this.model.get("ABUNDANCE"),
-				});
-			phases.push(primary);
-			
-			var componentIds = this.model.get("COMPONENTIDS");
-			var componentAcs = this.model.get("COMPONENTACRONYMS");
-			var componentAbs = this.model.get("COMPONENTAMOUNTS");
-			var nComponents = componentIds.length;
-			for (var iComponent = 0; iComponent < nComponents; iComponent++) {
-				var component = new Phase();
-				component.set({"PROTEINID": componentIds[iComponent],
-					"ACRONYM": componentAcs[iComponent],
-					"ABUNDANCE": componentAbs[iComponent],
-				});
-				phases.push(component);
-			}
-			
-			this.collection = phases;
+			this.collection = this.mudex.getComponents();
 			
 			Backbone.Validation.bind(this);
 			if (!this.model.has("NAME")) this.model.set({"NAME": "NULL"});
