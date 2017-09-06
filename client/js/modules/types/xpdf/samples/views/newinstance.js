a93389bf9276c2c2f9efdbc87f527b7f23657a73
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-09-06 12:25:36 +0100

    Change the New Instance view to more resemble the Instance view, and update experimental density on the fly.

--- client/js/modules/types/xpdf/samples/views/newinstance.js
+++ client/js/modules/types/xpdf/samples/views/newinstance.js
@@ -16,19 +16,40 @@ define([
 	return FormView.extend({
 		template: template,
 		
+		events: {
+			"change input.packingfraction": "onChangePacking",
+		},
+		
 		initialize: function(options) {
 			this.sampleName = options.sampleModel.get("NAME");
+			this.theoreticalDensity = options.sampleModel.get("THEORETICALDENSITY");
 		},
 		
 		createModel: function() {
 			this.model = new Instance();
-			this.model.set({"SAMPLENAME": this.sampleName});
+			this.model.set({
+				"SAMPLENAME": this.sampleName,
+				"PACKINGFRACTION": "1.0",
+				"THEORETICALDENSITY": this.theoreticalDensity,
+				"EXPERIMENTALDENSITY": this.theoreticalDensity,
+				});
 			
 			// Set default values for the container model values
 			this.model.set({"SIZE": "N/A",
 				"CONTAINERMATERIAL": "",
 				"CONTAINERDENSITY": "",
 			});
+		},
+		
+		onChangePacking: function(e) {
+			var valueStr = e.target.value;
+			var nyPackingFraction = Number.parseFloat(valueStr);
+			var theoreticalDensity = Number.parseFloat(this.model.get("THEORETICALDENSITY"));
+			var nyDensity = nyPackingFraction * theoreticalDensity;
+			this.model.set({
+				"PACKINGFRACTION": valueStr,
+				"EXPERIMENTALDENSITY": nyDensity});
+			this.render();
 		},
 		
 	});
