53386a3a5e585a7717eeac196f5cf48e6fd12505
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-09-08 12:19:14 +0100

    Initial functionality for choosing and displaying containers on a new instance.

--- client/js/modules/types/xpdf/samples/views/containerview.js
+++ client/js/modules/types/xpdf/samples/views/containerview.js
@@ -11,10 +11,16 @@ define([
 	){
 	
 	var ContainerView = Marionette.LayoutView.extend({
+		className: "container-view-inner",
 		template: template,
 
+		regions: {
+			containerDetails: "div.container-details",
+			encapContainer: "div.encapsulating",
+		},
+		
 		events: {
-//			"onchange select.container": selectContainer
+			"change select.container-select": "_selectContainer"
 		},
 		
 		/*
@@ -23,39 +29,91 @@ define([
 		 */
 		initialize: function(options) {
 			this.containers = options.containers;
-			
-			
-			this.model = new Backbone.Model({
-				// Set default values for the templated parameters 
-				SAMPLEDIMENSIONS: "",
-				PHASENAME: "",
-				PHASEDENSITY: "",
-				DIMENSION1: "",
-				SIZE1: "",
-				DIMENSION2: "",
-				SIZE2: "",
-				DIMENSION3: "",
-				SIZE3: "",
-			});
+			this.containerProperties = {};
 		},
 		
-		onBeforeRender: function() {
-			var containerSelect = this.$el.find("select.container");
+		onRender: function() {
 			
-			var nyOption = document.createElement("option");
-			// Add a Block Form as an option for the first level of containers
-			nyOption.text = "Block Form";
-			containerSelect.add(nyOption);
-			
-			var selectionAdder = addAllToSelect;
+//			if (this.select === undefined) {
+				// Get the elements associated with this container
+				// TODO Cope with multiple containers. Make sure to select only
+				// this container's elements
+				this.select = this.$el.find("select.container-select")[0];
+
+				var nyOption = document.createElement("option");
+				// Add a Block Form as an option for the first level of containers
+				nyOption.value= "block-form";
+				nyOption.text = "Block Form";
+				this.select.add(nyOption);
+				// Add Block Form properties (hard coded, currently)
+				this.containerProperties[nyOption.value] = {
+						"DIMENSION1": "Height",
+						"DIMENSION2": "Width",
+						"DIMENSION3": "Thickness",
+				};
+
+				addAllToSelect(this.containers, "", {select: this.select, properties: this.containerProperties});
+//			}
+		},
+		
+		_selectContainer: function(e) {
+			console.log("Selected container " + this.select.value);
+			var value = this.select.value;
+			if (value === "nothing") {
+				this.containerDetails.reset();
+			} else if (value.match(/-form$/)) {
+				this.containerDetails.show(new PseudoContainerView({properties: this.containerProperties[value]}));
+			} else {
+				this.containerDetails.show(new TrueContainerView({properties: this.containerProperties[value]}));
+			}
+		},
+	
+	});
+
+	var TrueContainerView = Marionette.LayoutView.extend({
+		className: "true-container",
+		
+		template: "<div class=\"form=\"><ul>" + 
+		"<li><span class=\"label\">Sample Dimensions</span><span class=\"SAMPLEDIMENSIONS\"><%=SAMPLEDIMENSIONS%></span>" + 
+		"<li><span class=\"label\">Container Material</span><span class=\"CONTMATERIAL\"><%=CONTMATERIAL%></span>" + 
+		"<li><span class=\"label\">Container Density</span><span class=\"CONTDENSITY\"><%=CONTDENSITY%></span>" + 
+		"</ul></div>",
+
+		initialize: function(options) {
+			console.log(options.properties);
+			this.model = new Backbone.Model(options.properties);
 		},
 	});
 	
+	var PseudoContainerView = Marionette.LayoutView.extend({
+		className: "pseudo-container",
+		
+		template: "<div class=\"form\"><ul>" + 
+		"<li><span class=\"label DIMENSION1\"><%DIMENSION1%></span><input type=\"text\" name=\"SIZE1\"></input> mm</li>" + 
+		"<li><span class=\"label DIMENSION1\"><%DIMENSION2%></span><input type=\"text\" name=\"SIZE2\"></input> mm</li>" + 
+		"<li><span class=\"label DIMENSION1\"><%DIMENSION3%></span><input type=\"text\" name=\"SIZE3\"></input> mm</li>" + 
+		"</ul></div>",
+		
+		initialize: function(options) {
+			console.log(options.properties);
+			this.model = new Backbone.Model(options.properties);
+		},
+		
+	});
+	
+	
 	var addAllToSelect = function(collection, response, options) {
 		var select = options.select;
+		var containerProperties = options.properties;
 		collection.each(function(element, index, collection) {
 			var nyOption = document.createElement("option");
 			nyOption.text = element.get("NAME");
+			// TODO get dynamically
+			containerProperties[element.get("NAME")] = {
+					"SAMPLEDIMENSIONS": "0.3 mm dia × 20 mm",
+					"CONTMATERIAL": "Quartz",
+					"CONTDENSITY": "2.6",
+			};
 			select.add(nyOption);
 		});
 	};
