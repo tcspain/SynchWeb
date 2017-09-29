f8657aa4a3a89ad2de0e7db7f1d571ad234d68cd
Timothy Spain <timothy.spain@diamond.ac.uk>
2017-09-29 15:56:34 +0100

    Fix the saving of sample groups

--- client/js/modules/types/xpdf/samples/collections/samplegroup.js
+++ client/js/modules/types/xpdf/samples/collections/samplegroup.js
@@ -34,24 +34,27 @@ define([
 		// BLSampleId is stored, then retrieve it from the saving of the first
 		// model.
 		save: function(options) {
+			console.log("SampleGroup.save:", this, options);
 			// No models? models deletions not done here
 			if (this.length < 1)
 				return;
 			// Special case for the first BLSample (the actual sample instance,
 			// usually), to get the BLSampleGroupId, if not yet defined.
-			var offset = 0;
+			this.offset = 0;
 			if (!this.groupId) {
 				var self = this;
-				this.at(offset).set({"ORDER": String(offset)});
-				this.at(offset).save({}, {
-					save: function(model, response, options) {
+				this.at(this.offset).set({"ORDER": String(this.offset)});
+				this.at(this.offset).save({}, {
+					success: function(model, response, options) {
 						self.groupId = model.get("BLSAMPLEGROUPID");
 						self.offset++;
+						console.log("SampleGroup.save.save:", self);
 						self._saveContainers();
 					},
 					error: function(model, response, options) {
 						console.log("Could not save sample to sample group: ", response);
 					},
+					type: "POST",
 				});
 			} else {
 				this._saveContainers();
@@ -67,19 +70,25 @@ define([
 		
 		// saves the containers from this.offset to the end of the list
 		_saveContainers: function() {
+			console.log("SampleGroup._saveContainers", this);
+			var groupId = this.groupId;
 			this.offset = this.reduce(function(offset, model, index, collection) {
+				if (index < offset)
+					return offset;
+				
 				model.set({
 					"ORDER": String(offset),
-					"BLSAMPLEGROUPID": this.groupId
+					"BLSAMPLEGROUPID": groupId
 					});
-
+				console.log("SampleGroup._saveContainers.reduce:", model, collection);
 				model.save({}, {
 					error: function(model, response, options) {
 						console.log("Could not save instance to sample group: ", response);
 					},
+					type: "POST",
 				});
-				return offset++;
-			});
+				return offset+1;
+			}, this.offset);
 		},
 		
 		
--- client/js/modules/types/xpdf/samples/models/samplegroupmember.js
+++ client/js/modules/types/xpdf/samples/models/samplegroupmember.js
@@ -11,9 +11,10 @@ define([
 		url: function() {
 			var baseUrl = "/sample/groups";
 			if (this.has("BLSAMPLEID")) {
-				baseUrl += "?" + this.get("BLSAMPLEID");
-				baseUrl += (this.has("TYPE")) ? "&" + this.get("TYPE") : "";
-				baseUrl += (this.has("ORDER")) ? "&" + this.get("ORDER") : "";
+				baseUrl += "?BLSAMPLEID=" + this.get("BLSAMPLEID");
+				baseUrl += (this.has("BLSAMPLEGROUPID")) ? "&BLSAMPLEGROUPID=" + this.get("BLSAMPLEGROUPID") : "";
+				baseUrl += (this.has("TYPE")) ? "&TYPE=" + this.get("TYPE") : "";
+				baseUrl += (this.has("ORDER")) ? "&ORDER=" + this.get("ORDER") : "";
 			}
 			return baseUrl;
 			
