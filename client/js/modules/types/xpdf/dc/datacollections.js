63f3bd1e201fafc898763d50c6ca20aca9de9585
Tim Spain <timothy.spain@diamond.ac.uk>
2017-11-03 11:42:32 +0000

    Clear out old and unused views and templates.

--- client/js/modules/types/xpdf/dc/datacollections.js
+++ client/js/modules/types/xpdf/dc/datacollections.js
@@ -4,14 +4,12 @@
 define([
         "modules/dc/datacollections",
         "modules/types/gen/dc/dclist",
-        "modules/types/xpdf/samples/views/newemptysample",
         "modules/types/xpdf/samples/views/newsamplefromphasepopup",
         "tpl!templates/types/xpdf/dc/dclist.html",
         ],
 function(
 		DataCollections,
 		DCList,
-		newEmptySample,
 		newSampleFromPhasePopupView,
 		template) {
 	return DataCollections.extend({
@@ -20,14 +18,5 @@ function(
 		filters: false,
 		sampleChanger: false,
 		
-		events : {
-            "click #new_sample": "newSample",
-		},
-	
-		newSample: function() {
-//			newEmptySample.run(this.model.get('VISIT'));
-			var newSampleView = new newSampleFromPhasePopupView({visitId: this.model.get("VISIT")});
-			app.dialog.show(newSampleView);
-		}
 	});
 });
